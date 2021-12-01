798bba715d0af4feb0ae5cf0cfa0096612133f3e
tomzig16 <tzigmantavicius@gmail.com>
2021-12-01 21:44:45 +0200

    Add Day1 (test)

---
+++ aoc21src/Day1/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Day1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> fileContents = AoC21Utils.SplitStringByNewLines(AoC21Utils.ReadFile("./input.txt"));
+            Console.WriteLine("Done reading");
+        }
+    }
+}
