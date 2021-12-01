0e61eae344806d3320366fb40d4dc171cbd8a55b
tomzig16 <tzigmantavicius@gmail.com>
2021-12-01 21:44:23 +0200

    Add solution and utils project

---
+++ aoc21src/Utils/AoC21Utils.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+public class AoC21Utils
+{
+    public static string ReadFile(string fileName)
+    {
+        string projectPath = Directory.GetParent(Environment.CurrentDirectory).Parent.Parent.FullName;
+        return System.IO.File.ReadAllText(projectPath + fileName).Trim();
+    }
+
+    public static List<string> SplitStringByNewLines(string content)
+    {
+        return content.Split("\n").ToList();
+    }
+}
