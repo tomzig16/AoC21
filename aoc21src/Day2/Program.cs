3347ca94a16f96712a01b2f3df6ce8eb549e86d7
tomzig16 <tzigmantavicius@gmail.com>
2021-12-02 10:50:46 +0200

    Solve Day 2 (Part 1 and Part 2)

---
+++ aoc21src/Day2/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Day2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> fileInput = AoC21Utils.SplitStringByNewLines(AoC21Utils.ReadFile("./input.txt"));
+
+            // Part 1
+            
+            const string kForw = "forward";
+            const string kDown = "down";
+            const string kUp = "up";
+
+            int horizontalPos = 0;
+            int depth = 0;
+            
+            foreach (var line in fileInput)
+            {
+                int value = int.Parse(line.Split(' ')[1]);
+                switch (line.Split(' ')[0])
+                {
+                    case kForw:
+                        horizontalPos += value;
+                        break;
+                    case kDown:
+                        depth += value;
+                        break;
+                    case kUp:
+                        depth -= value;
+                        break;
+                }
+            }
+
+            int result = horizontalPos * depth;
+            AoC21Utils.PrintResult(result.ToString());
+            
+            // Part 2
+
+            int aim = 0;
+            horizontalPos = 0;
+            depth = 0;
+            
+            foreach (var line in fileInput)
+            {
+                int value = int.Parse(line.Split(' ')[1]);
+                switch (line.Split(' ')[0])
+                {
+                    case kForw:
+                        horizontalPos += value;
+                        depth += (aim * value);
+                        break;
+                    case kDown:
+                        aim += value;
+                        break;
+                    case kUp:
+                        aim -= value;
+                        break;
+                }
+            }
+            
+            result = horizontalPos * depth;
+            AoC21Utils.PrintResult(result.ToString());
+        }
+    }
+}
--- aoc21src/Utils/AoC21Utils.cs
+++ aoc21src/Utils/AoC21Utils.cs
@@ -5,6 +5,7 @@ using System.Linq;
 
 public class AoC21Utils
 {
+    private static int partCounter = 1;
     public static string ReadFile(string fileName)
     {
         string projectPath = Directory.GetParent(Environment.CurrentDirectory).Parent.Parent.FullName;
@@ -19,5 +20,16 @@ public class AoC21Utils
     public static List<int> TransformStringInputToInts(List<string> input)
     {
         return input.Select(int.Parse).ToList();
+    }
+
+    public static void PrintResult(string result)
+    {
+        Console.WriteLine($"(Part {partCounter}): {result}");
+        partCounter++;
+    }
+    
+    public static void PrintResult(int part, string result)
+    {
+        Console.WriteLine($"(Part {part}): {result}");
     }
 }
